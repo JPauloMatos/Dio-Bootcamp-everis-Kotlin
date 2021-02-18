7ce4965bb5849146a40a1623ccdb076336d39c99
João Paulo Matos <lignanospeedy@gmail.com>
2021-02-17 21:34:38 -0300

    First Commit and Created person class

---
+++ fundamentos_oo/digionebank/README.md
@@ -0,0 +1,5 @@
+#BootCamp Everis Kotlin Developer Digital Innovation One
+
+##Fundamentos de Orientação a Objetos com Kotlin
+
+###digionebank
---
+++ fundamentos_oo/digionebank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.digionebank
+
+class Pessoa {
+    var nome:String = "João"
+    var cpf:String = "123.456.789-12"
+}
+
+fun main() {
+    val pessoa = Pessoa()
+
+    println(pessoa.nome)
+    println(pessoa.cpf)
+}
