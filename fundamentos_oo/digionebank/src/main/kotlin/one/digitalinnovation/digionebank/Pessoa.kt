f12ccc1b2b5a63502179b5a4614a2e0aa92cfbb6
João Paulo Matos <lignanospeedy@gmail.com>
2021-02-17 22:14:00 -0300

    Updated to talk about class members

--- fundamentos_oo/digionebank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/digionebank/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,14 +2,16 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome:String = "João"
-
     var cpf:String = "123.456.789-12"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val pessoa = Pessoa()
 
-    println(pessoa.nome)
-    println(pessoa.cpf)
+    println(pessoa.pessoaInfo())
 }
