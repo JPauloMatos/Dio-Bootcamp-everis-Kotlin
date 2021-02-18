fffd9ee8b702667d5115dc81b8b1135de67f9e83
João Paulo Matos <lignanospeedy@gmail.com>
2021-02-18 02:34:39 -0300

    last commit for the object oriented class with kotlin

--- fundamentos_oo/digionebank/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ fundamentos_oo/digionebank/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -3,7 +3,9 @@ package one.digitalinnovation.digionebank.testes
 import one.digitalinnovation.digionebank.Gerente
 
 fun main() {
-    val maria = Gerente(nome = "Maria do Carmo", cpf = "123.098.765-42", salario = 5000.0)
+    val maria = Gerente(nome = "Maria do Carmo", cpf = "123.098.765-42", salario = 5000.0, senha = "senha12")
 
     ImprimeRelatorioFuncionario.imprime(maria)
+
+    TesteAutenticacao().autentica(maria)
 }
